00cb9ff543e8865aa48c24719e30c9e00010faeb
okunlade Jolade <jokunlade@gmail.com>
2022-06-01 09:49:42 +0100

    my first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="" alt="" />
+        <ul>
+          <li>ABOUT US</li>
+          <li>BLOG</li>
+          <li>CONTACT US</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
