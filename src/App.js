bc86070b95d9f8727afc2a6725b8f8a2e1460b01
okunlade Jolade <jokunlade@gmail.com>
2022-06-01 11:44:56 +0100

    setting up the landing page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react"
 import "./images/metricks-white.png"
 import './App.css';
+import Nav from "./components/nav"
+import About from "./components/about"
+import Contact from "./components/contact"
+import Blog from "./components/blog"
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="./images/metricks-white.png" alt="Metricks" />
-        <ul>
-          <li>ABOUT US</li>
-          <li>BLOG</li>
-          <li>CONTACT US</li>
-        </ul>
-      </header>
+      <Nav />
+      <About />
+      <Contact />
+      <Blog />
     </div>
   );
 }
---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+
+function Nav() {
+  return (
+    <nav>
+        <img className="logo" src="../images/metricks-white.png" alt="Metricks" />
+    <ul className="nav-links">
+        <li>ABOUT US</li>
+        <li>BLOG</li>
+        <li>CONTACT US</li>
+    </ul>
+    
+    </nav>
+     
+  );
+}
+
+export default Nav;
